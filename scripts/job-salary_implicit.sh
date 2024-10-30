33e331ffa6de61fd854df37ae56a202cfb6d7b70
makiart <haruki3968@gmail.com>
2024-10-30 10:48:18 +0900

    remove: some files

--- scripts/job-salary_implicit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python experiments/sensitive_attribute_synthetic_dataset/synthetic_salary_implicit/main.py \
-    encoder.dpr=false encoder.batch_size=512 benchmark.execlude_non_numeric=false benchmark.depth=500
